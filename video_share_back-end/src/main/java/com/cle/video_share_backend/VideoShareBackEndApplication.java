33bab809eca2970c8445b2cf4002efe26c6e5d98
ccccle <1175414205@qq.com>
2023-09-28 13:12:39 +0800

    初始化前后端

---
+++ video_share_back-end/src/main/java/com/cle/video_share_backend/VideoShareBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.cle.video_share_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoShareBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoShareBackEndApplication.class, args);
+    }
+
+}
---
+++ video_share_back-end/src/test/java/com/cle/video_share_backend/VideoShareBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cle.video_share_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoShareBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
